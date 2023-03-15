ebdc39cb25b55fd32fad30b0cb4033f87bec252a
Alex Malyk <oleksiimal@wizardsdev.com>
2023-03-15 22:48:08 +0200

    fix app titles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function Home() {
   return (
     <>
-      <AppHead title="company_name" desription="description_text" />
+      <AppHead title="company_name" desription="description_text" isMain />
       <MainPage />
     </>
   );
