a1076ec1035d1f8cfc51a18e73e828e3d10668ba
Alex Malyk <oleksiimal@wizardsdev.com>
2023-03-28 22:21:02 +0300

    fix footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -16,24 +16,26 @@ const Footer: React.FC = () => {
   };
   return (
     <StFooter>
-      <StFlex flexDirection="column" ai="start" marginBottom="30px">
-        <Icon alt="logo" name="white_logo" ext="png" />
-        <StSpan margin="20px 0" {...commonProps}>{locales.footer_description}</StSpan>
-        <StSpan marginBottom="20px" {...commonProps}>{locales.footer_address}</StSpan>
-        <StLink href={`tel:${locales.tel}`} {...commonProps}>{locales.phone}</StLink>
-      </StFlex>
-      <StFlex flexDirection="column" jc="space-between" justifySelf="center" ai="start" marginBottom="30px">
-        <StSpan color="white" fontSize="24px" fontWeight={FontWeight.bold}>{locales.menu}</StSpan>
-        <Link margin="20px 0" href="products" {...commonProps}>{locales.products}</Link>
-        <Link href="about_us" {...commonProps}>{locales.about_us}</Link>
-        <Link margin="20px 0" href="faq" {...commonProps}>{locales.faq}</Link>
-        <Link href="contact" {...commonProps}>{locales.contact}</Link>
-      </StFlex>
-      <StFlex flexDirection="column" justifySelf="end" ai="start">
-        <StSpan color="white" fontSize="24px" fontWeight={FontWeight.bold}>{locales.stay_connected}</StSpan>
-        <Link target="_blank" margin="20px 0" href="facebook" {...commonProps}>{locales.facebook}</Link>
-        <Link href="instagram" {...commonProps}>{locales.instagram}</Link>
-      </StFlex>
+      <div className="container footer-container">
+        <StFlex flexDirection="column" ai="start" marginBottom="30px">
+          <Icon alt="logo" name="white_logo" ext="png" />
+          <StSpan margin="20px 0" {...commonProps}>{locales.footer_description}</StSpan>
+          <StSpan marginBottom="20px" {...commonProps}>{locales.footer_address}</StSpan>
+          <StLink href={`tel:${locales.tel}`} {...commonProps}>{locales.phone}</StLink>
+        </StFlex>
+        <StFlex flexDirection="column" jc="space-between" justifySelf="center" ai="start" marginBottom="30px">
+          <StSpan color="white" fontSize="24px" fontWeight={FontWeight.bold}>{locales.menu}</StSpan>
+          <Link margin="20px 0" href="products" {...commonProps}>{locales.products}</Link>
+          <Link href="about_us" {...commonProps}>{locales.about_us}</Link>
+          <Link margin="20px 0" href="faq" {...commonProps}>{locales.faq}</Link>
+          <Link href="contact" {...commonProps}>{locales.contact}</Link>
+        </StFlex>
+        <StFlex flexDirection="column" justifySelf="end" ai="start">
+          <StSpan color="white" fontSize="24px" fontWeight={FontWeight.bold}>{locales.stay_connected}</StSpan>
+          <Link target="_blank" margin="20px 0" href="facebook" {...commonProps}>{locales.facebook}</Link>
+          <Link href="instagram" {...commonProps}>{locales.instagram}</Link>
+        </StFlex>
+      </div>
     </StFooter>
   );
 };
--- src/components/Footer/styled.ts
+++ src/components/Footer/styled.ts
@@ -3,16 +3,22 @@ import styled from 'styled-components';
 
 export const StFooter = styled.footer`
   background: ${colors.brownPrimary};
-  padding: 54px 73px 40px;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  
+  .footer-container {
+    padding: 54px 73px 40px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
   @media print {
     display: none;
   }
   @media (max-width: 550px) {
-  display:flex;
-  flex-direction:column;
-  align-items: center;
+    .footer-container {
+      display:flex;
+      flex-direction:column;
+      align-items: center;
+      & > div {
+        align-items: center !important;
+      }
+    }
   }  
 `;
