30e2b1e28b0b334423ba73d7b8d8a6e119a01325
Alex Malyk <oleksiimal@wizardsdev.com>
2023-04-05 23:53:56 +0300

    add google tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { GlobalStyle } from '@/components/common/styled/Global';
 import type { AppProps } from 'next/app';
-import React from 'react';
 import Modals from '@/components/Modal/Modals';
+import { initializeTagManager, trackPageView } from '@/helpers/tagManager';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
+  useEffect(initializeTagManager, []);
+  useEffect(() => {
+    trackPageView(pathname);
+  }, [pathname]);
   return (
     <>
       <GlobalStyle />
